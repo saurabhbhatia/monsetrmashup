f182b7359fff8e84efc4ee1e7a5f7b4ee31934f2
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-07-03 21:16:02 +1000

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <div class="container">
+      <div class="content is-medium">
+        <img src="https://res.cloudinary.com/drg9hguhu/image/upload/v1530615614/saurabh/monstermash.jpg">
+        <a class="button is-large center-button">Coming Soon</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/shared/NavBar'
+export default {
+  name: 'Home',
+  components: {
+    'navbar': NavBar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/shared/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="navbar is-white">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">Monster Mash-Up - A PDX Anthology</a>
+      <div class="navbar-burger burger" data-target="top-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="top-menu" class="navbar-menu">
+      <div class="navbar-end">
+        <a class="navbar-item" href="">About</a>
+        <a class="navbar-item" href="">Contributors</a>
+        <a class="navbar-item" href="">Characters</a>
+        <a class="navbar-item" href="">Campaign</a>
+        <a class="navbar-item" href="">Contact</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
