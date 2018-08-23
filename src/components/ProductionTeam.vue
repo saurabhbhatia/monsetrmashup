ce6e7e2b7d8c7f044c5c92e0eea74031633972a6
Saurabh Bhatia <saurabh.a.bhatia@gmail.com>
2018-08-23 21:00:41 +1000

    add rosa's picture

--- src/components/ProductionTeam.vue
+++ src/components/ProductionTeam.vue
@@ -57,7 +57,7 @@
           <div class="card">
             <div class="card-image">
               <figure class="image is-4by3">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                <img src="https://res.cloudinary.com/drg9hguhu/image/upload/v1535021724/rosa_eibv1s.jpg" alt="Placeholder image">
               </figure>
             </div>
             <div class="card-content">
